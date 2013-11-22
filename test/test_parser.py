acfaa32f1b75d654cc6e523871864fb1ea4c1324
barnabywalters <barnaby@waterpigs.co.uk>
2013-11-22 15:16:06 +0000

    Added tests, fixtures for multiple root classnames, simple properpty nesting, implied properties

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,5 +1,10 @@
 import nose
+import os.path
 from mf2py.parser import Parser
+
+def parse_fixture(path):
+    p = Parser(open(os.path.join("test/examples/", path)))
+    return p.to_dict()
 
 def test_empty():
    p = Parser() 
@@ -44,3 +49,36 @@ def test_person_with_url():
     assert result["items"][0]["properties"]["name"] == [u'Tom Morris']
     assert result["items"][0]["properties"]["url"] == [u'http://tommorris.org/']
 
+def test_multiple_root_classnames():
+    result = parse_fixture("nested_multiple_classnames.html")
+    # order does not matter
+    assert len(result["items"]) == 1
+    assert set(result["items"][0]["type"]) == set(["h-entry", "h-as-note"])
+
+def test_property_nested_microformat():
+    result = parse_fixture("nested_multiple_classnames.html")
+    
+    assert len(result["items"]) == 1
+    assert "author" in result["items"][0]["properties"]
+    assert result["items"][0]["properties"]["author"][0]["properties"]["name"][0] == "Tom Morris"
+
+def test_implied_name():
+    result = parse_fixture("implied_properties.html")
+    assert result["items"][0]["properties"]["name"][0] == "Tom Morris"
+
+def test_implied_url():
+    result = parse_fixture("implied_properties.html")
+    assert result["items"][1]["properties"]["url"][0] == "http://tommorris.org/"
+
+def test_implied_nested_photo():
+    result = parse_fixture("implied_properties.html")
+    assert result["items"][2]["properties"]["photo"][0] == "http://tommorris.org/photo.png"
+
+def test_implied_nested_photo_alt_name():
+    result = parse_fixture("implied_properties.html")
+    assert result["items"][3]["properties"]["name"][0] == "Tom Morris"
+
+def test_implied_image():
+    result = parse_fixture("implied_properties.html")
+    assert result["items"][4]["properties"]["photo"][0] == "http://tommorris.org/photo.png"
+    assert result["items"][4]["properties"]["name"][0] == "Tom Morris"
