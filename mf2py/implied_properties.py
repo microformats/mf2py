d91042b34e3b48505f15088dce82ec10d6a45bf6
Kartik Prabhu <katsdaway@gmail.com>
2014-03-01 12:40:00 -0600

    Update implied_properties.py
    changed to use the get_attr method

--- mf2py/implied_properties.py
+++ mf2py/implied_properties.py
@@ -1,20 +1,37 @@
-from bs4 import BeautifulSoup
 import mf2_classes
+from dom_helpers import get_attr 
 
 ## function to find an implied name property
 def name(el):
     # if image use alt text if not empty
-    if el.name == 'img' and "alt" in el.attrs and not el["alt"] == "":
-        return [el["alt"]]
+    prop_value = get_attr(el, "alt", check_name="img")
+    if prop_value is not None:
+        return [prop_value]
+
     # if abbreviation use the title if not empty
-    if el.name == 'abbr' and "title" in el.attrs and not el["title"] == "":
-        return [el["title"]]
+    prop_value = get_attr(el, "title", check_name="abbr")
+    if prop_value is not None:
+        return [prop_value]
+
     # if only one image child then use alt text if not empty
-    if len(el.find_all("img")) == 1 and "alt" in el.find_all("img")[0].attrs and not el.find_all("img")[0]["alt"] == "":
-        return [el.find_all("img")[0]["alt"]]
+    poss_imgs = el.find_all("img")
+    if len(poss_imgs) == 1:
+        poss_img = poss_imgs[0]
+
+        prop_value = get_attr(poss_img, "alt", check_name="img")
+        if prop_value is not None:
+            return [prop_value]
+
+
     # if only one abbreviation child use abbreviation if not empty
-    if len(el.find_all("abbr")) == 1 and "title" in el.find_all("abbr")[0].attrs and not el.find_all("abbr")[0]["title"] == "":
-        return [el.find_all("abbr")[0]["title"]]
+    poss_abbrs = el.find_all("abbr")
+    if len(poss_abbrs) == 1:
+        poss_abbr = poss_abbrs[0]
+
+        prop_value = get_attr(poss_abbr, "title", check_name="abbr")
+        if prop_value is not None:
+            return [prop_value]
+
     # TODO(tommorris): implement the rest of http://microformats.org/wiki/microformats2-parsing#parsing_for_implied_properties
     # use text if all else fails
     return [el.get_text().strip()]
@@ -22,20 +39,37 @@ def name(el):
 ## function to find implied photo property
 def photo(el):
     # if element is an image use source if exists
-    if el.name == 'img' and "src" in el.attrs:
-        return [el["src"]]
+    prop_value = get_attr(el, "src", check_name="img")
+    if prop_value is not None:
+        return [prop_value]
+
     # if element has one image child use source if exists (check existence of src?)
-    if len(el.find_all("img")) == 1 and "src" in el.find_all("img")[0].attrs :
-        return [el.find_all("img")[0]["src"]]
+    poss_imgs = el.find_all("img")
+    if len(poss_imgs) == 1:
+        poss_img = poss_imgs[0]
+
+        prop_value = get_attr(poss_img, "src", check_name="img")
+        if prop_value is not None:
+            return [prop_value]
+
+
     # TODO(tommorris): implement the other implied photo finders from http://microformats.org/wiki/microformats2-parsing#parsing_for_implied_properties
     return None
 
 ## function to find implied url
 def url(el):
     # if element is a link use its href if exists
-    if el.name == 'a' and "href" in el.attrs:
-        return [el["href"]]
+    prop_value = get_attr(el, "href", check_name="a")
+    if prop_value is not None:
+        return [prop_value]
+
     # if one link child use its href 
-    if len(el.find_all("a")) == 1 and "href" in el.find_all("a")[0].attrs and mf2_classes.root(el.find_all("a")[0].get('class',[])) == []:
-        return [el.find_all("a")[0]["href"]]
+    poss_as = el.find_all("a")
+    if len(poss_as) == 1:
+        poss_a = poss_as[0]
+
+        prop_value = get_attr(poss_a, "href", check_name="a")
+        if prop_value is not None and mf2_classes.root(poss_a.get('class',[])) == []:
+            return [prop_value]
+
     return None 
