74f81eb97f7116db184bc6749999c6fb2d61b4e1
Kartik Prabhu <katsdaway@gmail.com>
2014-03-04 19:40:15 -0600

    add absolute url maker
    url property now normalises to a given base url. referring to #12

--- mf2py/parse_property.py
+++ mf2py/parse_property.py
@@ -1,5 +1,6 @@
 from bs4 import Tag
 from dom_helpers import get_attr
+from urlparse import urljoin
 ## functions to parse the propertis of elements
 
 def text(el):
@@ -20,19 +21,19 @@ def text(el):
     # strip here?
     return el.get_text()
 
-def url(el):
+def url(el, base_url=''):
     ## do the normalise absolute url thing
     prop_value = get_attr(el, "href", check_name=("a","area"))
     if prop_value is not None:
-        return prop_value
+        return urljoin(base_url, prop_value)
 
     prop_value = get_attr(el, "src", check_name="img")
     if prop_value is not None:
-        return prop_value
+        return urljoin(base_url, prop_value)
 
     prop_value = get_attr(el, "data", check_name="object")
     if prop_value is not None:
-        return prop_value
+        return urljoin(base_url, prop_value)
 
     # add value-class-pattern
 
