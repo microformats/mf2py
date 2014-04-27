87b3b8c0f4f02ce2c4f4d22db04f4a7e9aa4801f
Kartik Prabhu <kartikprabhu@users.noreply.github.com>
2014-04-27 17:03:11 -0500

    fix dom_helpers. check for check_name to be tuple/list

--- mf2py/dom_helpers.py
+++ mf2py/dom_helpers.py
@@ -8,13 +8,13 @@ else:
 
 
 def get_attr(el, attr, check_name=None ):
-    """returns the attribute of an element if it exists and is not empty else returns None. Optional kwarg 'check_name' (as a list of strings or single string) to check element type by name
+    """returns the attribute of an element if it exists and is not empty else returns None. Optional kwarg 'check_name' (as a list/tuple of strings or single string) to check element type by name
     """
     if check_name is None:
         attr_value = el.get(attr, None)
     elif isinstance(check_name, basestring) and el.name == check_name:
         attr_value = el.get(attr, None)
-    elif el.name in check_name:
+    elif (isinstance(check_name, tuple) or isinstance(check_name, list)) and el.name in check_name:
         attr_value = el.get(attr, None)
     else:
         attr_value = None
