bc1ce02e3c60ec022c8ebbe3408efdde82376c01
Tom Morris <tom@tommorris.org>
2014-09-07 16:22:29 +0100

    pretty print + GET

--- mf2py/http_server.py
+++ mf2py/http_server.py
@@ -7,17 +7,17 @@ app = Flask(__name__)
 def index():
     resp = """<!DOCTYPE HTML>
     <html><head><title>mf2py</title></head>
-    <body><form action="/parse" method="post">
+    <body><form action="/parse" method="get">
     <h1>mf2py test</h1>
     URL: <input type="text" name="url" /> <input type="submit" /></body></html>"""
     return Response(resp, status=200, mimetype="text/html")
 
-@app.route("/parse", methods=["POST"])
+@app.route("/parse", methods=["GET", "POST"])
 def parse():
     print request
     u = request.form['url']
     p = Parser(url=unicode(u))
-    return Response(p.to_json(), status=200, mimetype='application/json')
+    return Response(p.to_json(pretty_print=True), status=200, mimetype='application/json')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 33507)))
