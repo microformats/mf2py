a2ca07599eb5e10bcba4d707b4b9a3897dfed462
Tom Morris <tom@tommorris.org>
2014-09-07 15:42:43 +0100

    adding simple HTTP server for testing

---
+++ mf2py/http_server.py
@@ -0,0 +1,23 @@
+from mf2py import Parser
+from flask import Flask, Response, request
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    resp = """<!DOCTYPE HTML>
+    <html><head><title>mf2py</title></head>
+    <body><form action="/parse" method="post">
+    <h1>mf2py test</h1>
+    URL: <input type="text" name="url" /> <input type="submit" /></body></html>"""
+    return Response(resp, status=200, mimetype="text/html")
+
+@app.route("/parse", methods=["POST"])
+def parse():
+    print request
+    u = request.form['url']
+    p = Parser(url=unicode(u))
+    return Response(p.to_json(), status=200, mimetype='application/json')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
