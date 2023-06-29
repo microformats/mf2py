c5c69cb96adbda6eae8f13e68d32baa8db744ca9
capjamesg <jamesg@jamesg.blog>
2023-06-29 19:25:41 +0100

    run black and isort on dumpout file

--- dumpout.py
+++ dumpout.py
@@ -1,10 +1,11 @@
-from mf2py import Parser
-import os.path
 import glob
 import json
+import os.path
 
-allfiles = glob.glob(os.path.join('.', 'tests', 'tests', '**', '**', '*.json'))
+from mf2py import Parser
+
+allfiles = glob.glob(os.path.join(".", "tests", "tests", "**", "**", "*.json"))
 for jsonfile in allfiles:
-    htmlfile = jsonfile[:-4] + 'html'
+    htmlfile = jsonfile[:-4] + "html"
     with open(htmlfile) as f:
         p = json.loads(Parser(doc=f).to_json(pretty_print=True))
