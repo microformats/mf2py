ef0677ff535809997590154fd63a38e97dcb11f8
Ryan Barrett <git@ryanb.org>
2023-11-28 11:04:14 -0800

    metaformats: add Twitter card backcompat meta tags

--- mf2py/metaformats.py
+++ mf2py/metaformats.py
@@ -6,27 +6,36 @@ from . mf2_classes import filter_classes
 from .dom_helpers import try_urljoin
 
 
-OGP_TO_MF2 = {
-    "article:author": "author",
-    "article:published_time": "published",
-    "article:modified_time": "updated",
-    "og:audio": "audio",
-    "og:description": "summary",
-    "og:image": "photo",
-    "og:title": "name",
-    "og:video": "video",
-}
+METAFORMAT_TO_MF2 = [
+    # in priority order, descending
+
+    # OGP
+    ("article:author", "author"),
+    ("article:published_time", "published"),
+    ("article:modified_time", "updated"),
+    ("og:audio", "audio"),
+    ("og:description", "summary"),
+    ("og:image", "photo"),
+    ("og:title", "name"),
+    ("og:video", "video"),
+
+    # Twitter
+    ("twitter:title", "name"),
+    ("twitter:description", "summary"),
+    ("twitter:image", "photo"),
+]
 OGP_TYPE_TO_MF2 = {
     "article": "h-entry",
     "movie": "h-cite",
     "music": "h-cite",
     "profile": "h-card",
 }
-OGP_URL_PROPERTIES = {
+URL_PROPERTIES = {
     "article:author",
     "og:audio",
     "og:image",
     "og:video",
+    "twitter:image",
 }
 
 
@@ -49,17 +58,23 @@ def parse(soup, url=None):
 
     parsed = {}
 
+    # Properties
+    for meta, mf2 in METAFORMAT_TO_MF2:
+        if val := soup.head.find("meta", property=meta):
+            if content := val.get("content"):
+                if meta in URL_PROPERTIES:
+                    content = try_urljoin(url, content)
+                parsed.setdefault("properties", {}).setdefault(mf2, [content])
+
+    if not parsed:
+        # No OGP or Twitter properties
+        return None
+
+    # type from OGP or default to h-entry
+    parsed["type"] = ["h-entry"]
     if ogp_type := soup.head.find("meta", property="og:type"):
         if content := ogp_type.get("content"):
             if mf2_type := OGP_TYPE_TO_MF2.get(content.split(".")[0]):
                 parsed["type"] = [mf2_type]
 
-    for ogp, mf2 in OGP_TO_MF2.items():
-        if val := soup.head.find("meta", property=ogp):
-            if content := val.get("content"):
-                if ogp in OGP_URL_PROPERTIES:
-                    content = try_urljoin(url, content)
-                print(ogp, mf2, val, content)
-                parsed.setdefault("properties", {})[mf2] = [content]
-
-    return parsed or None
+    return parsed
